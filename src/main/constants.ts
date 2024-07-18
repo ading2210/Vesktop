273f8af3c3b52e406e6890340ec61b094a3aa359
Sammie Zhang <101011086+sz6084@users.noreply.github.com>
2024-07-18 13:02:28 -0700

    Update constants.ts
    Part of resolving conflicts

--- src/main/constants.ts
+++ src/main/constants.ts
@@ -48,7 +48,8 @@ export const VENCORD_THEMES_DIR = join(DATA_DIR, "themes");
 // needs to be inline require because of circular dependency
 // as otherwise "DATA_DIR" (which is used by ./settings) will be uninitialised
 export const VENCORD_FILES_DIR =
-    (require("./settings") as typeof import("./settings")).Settings.store.vencordDir || join(DATA_DIR, "vencordDist");
+    (require("./settings") as typeof import("./settings")).State.store.vencordDir ||
+    join(SESSION_DATA_DIR, "vencordFiles");
 
 export const USER_AGENT = `Vesktop/${app.getVersion()} (https://github.com/Vencord/Vesktop)`;
 
